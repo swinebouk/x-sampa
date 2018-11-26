eb3afb999909098eaf03b7e05351fe816f7b07ea
Johnny Lee-Othon <jleeothon@icloud.com>
2018-11-27 00:35:30 +0100

    fix test/translate.js

--- test/translate.js
+++ test/translate.js
@@ -1,10 +1,38 @@
 import test from 'ava';
-import tokenize from '../lib/tokenize';
+import render from '../lib/translate';
 
-function testTokenizer(t, input, output) {
-	t.deepEqual(tokenize(input), output);
+function testRender(t, input, output) {
+	t.deepEqual(render(input), output);
 }
 
-test('Single character', testTokenizer, 't', ['t']);
-test('Three distinct symbols inside slashes', testTokenizer, '/aba/', ['/', 'a', 'b', 'a', '/']);
-test('Character with backslashes', testTokenizer, 'J\\aj', ['J\\', 'a', 'j']);
+test('Single character', testRender, 't', 't');
+test(
+	'Render other symbols',
+	testRender,
+	'/aba/',
+	'/aba/'
+);
+test(
+	'Render char with trailing slash (J\\ -> ɟ)',
+	testRender,
+	'J\\',
+	'ɟ'
+);
+test(
+	'Render char with modifier (o~ -> o)',
+	testRender,
+	'o~',
+	'õ'
+);
+test(
+	'Character with modifier without descender /oʊ̯, haɪ̯/',
+	testRender,
+	'oU_^, haI_^',
+	'oʊ̯, haɪ̯'
+);
+test(
+	'Character with modifier with descender /ɪk bən lœy̑k/',
+	testRender,
+	'Ik b@n l9y_^k',
+	'ɪk bən lœy̑k'
+);
