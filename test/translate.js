3a9891e22af56b7089f0cdc040a5026d910186a4
Johnny Lee-Othon <jleeothon@icloud.com>
2019-03-10 20:07:46 +0100

    redo modifier letters

--- test/translate.js
+++ test/translate.js
@@ -1,38 +1,43 @@
+import {readFileSync} from 'fs';
+import {join} from 'path';
 import test from 'ava';
-import render from '../lib/translate';
 
-function testRender(t, input, output) {
-	t.deepEqual(render(input), output);
+import translate from '../lib/translate';
+
+function readFile(fileName) {
+	return readFileSync(join(__dirname, fileName), {encoding: 'utf-8'});
 }
 
-test('Single character', testRender, 't', 't');
-test(
-	'Render other symbols',
-	testRender,
-	'/aba/',
-	'/aba/'
-);
-test(
-	'Render char with trailing slash (J\\ -> ɟ)',
-	testRender,
-	'J\\',
-	'ɟ'
-);
-test(
-	'Render char with modifier (o~ -> o)',
-	testRender,
-	'o~',
-	'õ'
-);
-test(
-	'Character with modifier without descender /oʊ̯, haɪ̯/',
-	testRender,
-	'oU_^, haI_^',
-	'oʊ̯, haɪ̯'
-);
-test(
-	'Character with modifier with descender /ɪk bən lœy̑k/',
-	testRender,
-	'Ik b@n l9y_^k',
-	'ɪk bən lœy̑k'
-);
+function testTranslate(t, input, output) {
+	t.deepEqual(translate(input), output);
+}
+
+test('Single character', testTranslate, 't', 't');
+
+test('Other symbols', testTranslate, '/aba/', '/aba/');
+
+test('Trailing slash', testTranslate, 'J\\', 'ɟ');
+
+test('Nasal vowel', testTranslate, 'o~', 'õ');
+
+// Syllabicity
+
+test('Syllabic', testTranslate, 'n=N=', 'n̩ŋ̍');
+
+test('Non-syllabic', testTranslate, 'I_^y_^', 'ɪ̯y̑');
+
+// Consonant release
+
+test('Unreleased stop', testTranslate, 'p_}', 'p̚')
+
+test('Aspirated stop', testTranslate, 'p_h', 'pʰ');
+
+test('Nasalised release', testTranslate, 'p_n', 'pⁿ');
+
+test('Lateral release', testTranslate, 'd_l', 'dˡ');
+
+// Voicing
+
+// Other
+
+test('The Sun and the North Wind (English)', testTranslate, readFile('en-us-input.txt'), readFile('en-us-output.txt'));
